388102b190f3677ecad008628a5db4be3cfe3064
Joe Milham <jmilham21@gmail.com>
2022-07-06 13:10:21 +0100

    Explicitly control number of offscreen pages for viewpager

--- app/src/main/java/com/jmilham/scrollingfeed/ui/main/MainFragment.kt
+++ app/src/main/java/com/jmilham/scrollingfeed/ui/main/MainFragment.kt
@@ -9,7 +9,6 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager2.widget.ViewPager2
 import com.jmilham.scrollingfeed.R
-import com.jmilham.scrollingfeed.models.JwAdvertisement
 import com.jmilham.scrollingfeed.ui.helpers.VideoFragmentAdapter
 
 class MainFragment : Fragment() {
@@ -35,15 +34,18 @@ class MainFragment : Fragment() {
         pager = view.findViewById(R.id.pager)
         adapter = VideoFragmentAdapter(this, ArrayList())
         pager?.adapter = adapter
+        pager?.offscreenPageLimit = 1 // this is a danger number if higher (and sometimes even at 1)
+        // BE CAUTIOUS OF ABOVE AND THE AMOUNT OF DECODERS THE DEVICE FOR THE MIME TYPE.
+        // there are two limitations for how large this should be: decoders and memory allocation
         viewModel.liveVideos.observe(viewLifecycleOwner) {
             if (it.isEmpty()) {
                 // bad playlistID or bad config
                 Toast.makeText(context, "PlaylistID invalid", Toast.LENGTH_LONG).show()
             } else {
-                it.add(it.size / 2, JwAdvertisement("https://playertest.longtailvideo.com/vast/preroll-jw.xml")) // add an ad to the middle for test
+                // TODO: Refactor how to insert an ad type into the list
+//                it.add(it.size / 2, JwAdvertisement("https://playertest.longtailvideo.com/vast/preroll-jw.xml")) // add an ad to the middle for test
                 adapter = VideoFragmentAdapter(this, it)
                 pager?.adapter = adapter
-                pager?.offscreenPageLimit = 4
                 adapter.notifyDataSetChanged() // safe since only on fresh load
             }
         }
