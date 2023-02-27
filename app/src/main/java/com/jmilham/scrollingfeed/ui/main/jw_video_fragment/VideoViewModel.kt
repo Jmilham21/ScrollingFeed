5157132fc5dc223e7f5a8545ccbad3a58795fc5c
Joe Milham <jmilham21@gmail.com>
2023-02-27 18:52:38 +0100

    Added loading icon. Added block of ui interaction while loading. Added logic for thumbnail display while loading. seek to start on pagination.

--- app/src/main/java/com/jmilham/scrollingfeed/ui/main/jw_video_fragment/VideoViewModel.kt
+++ app/src/main/java/com/jmilham/scrollingfeed/ui/main/jw_video_fragment/VideoViewModel.kt
@@ -1,6 +1,44 @@
 package com.jmilham.scrollingfeed.ui.main.jw_video_fragment
 
 import androidx.lifecycle.ViewModel
+import com.jmilham.scrollingfeed.models.JwAdvertisement
+import com.jmilham.scrollingfeed.models.JwVideo
+import com.jwplayer.pub.api.configuration.PlayerConfig
+import com.jwplayer.pub.api.configuration.UiConfig
+import com.jwplayer.pub.api.configuration.ads.ima.ImaAdvertisingConfig
+import com.jwplayer.pub.api.media.ads.AdBreak
 
 class VideoViewModel : ViewModel() {
+    fun getPlayerConfig(media: JwVideo): PlayerConfig {
+        val allDisabledUiConfig = UiConfig.Builder().hideAllControls().build()
+        return PlayerConfig.Builder()
+            .file("https://cdn.jwplayer.com/manifests/${media.mediaid}.m3u8")
+            .image("https://cdn.jwplayer.com/v2/media/${media.mediaid}/poster.jpg")
+            .stretching(PlayerConfig.STRETCHING_FILL) // allow the media to fill allotted space as best as possible
+            .preload(true) // data hog and may be overkill
+            .autostart(true)
+            .uiConfig(allDisabledUiConfig)
+            .repeat(true)
+            .build()
+    }
+
+    fun getPlayerAdvertConfig(advertisement: JwAdvertisement): PlayerConfig {
+        val adSchedule: MutableList<AdBreak> = ArrayList()
+        val adBreak: AdBreak = AdBreak.Builder()
+            .offset("pre")
+            .tag(advertisement.adUrl)
+            .build()
+        adSchedule.add(adBreak)
+        val imaAdvertising: ImaAdvertisingConfig = ImaAdvertisingConfig.Builder()
+            .schedule(adSchedule)
+            .build()
+
+        val allDisabledUiConfig = UiConfig.Builder().hideAllControls().build()
+        return PlayerConfig.Builder()
+            .file("http://content.jwplatform.com/videos/nhYDGoyh-el5vTWpr.mp4") // TODO: how do i setup a config without a file / media and just do an ad?
+            .stretching(PlayerConfig.STRETCHING_UNIFORM) // allow the media to fill allotted space as best as possible
+            .advertisingConfig(imaAdvertising)
+            .uiConfig(allDisabledUiConfig)
+            .build()
+    }
 }
